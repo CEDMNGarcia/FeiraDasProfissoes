7bc8a5a6582c89e43bb64353ba95353fd0fe1959
devCarlosGacia <cemngarcia@gmail.com>
2025-03-09 16:50:54 -0300

    telas prontas: home, galeria | a iniciar: objetivos, cadastre-se

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,  // Mostra 1 slide por vez
+        spaceBetween: 10,  // Espaçamento entre os slides
+        pagination: {
+            el: ".swiper-pagination",  // Definindo onde a paginação vai aparecer
+            clickable: true,  // Permitir que a paginação seja clicável
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",  // Botão "Próximo"
+            prevEl: ".swiper-button-prev",  // Botão "Anterior"
+        },
+        autoplay: {
+            delay: 3000,  // Define o intervalo de 4 segundos (4000ms)
+            disableOnInteraction: false,  // Faz com que o autoplay continue mesmo após interação com o carrossel
+        },
+    });
+});
